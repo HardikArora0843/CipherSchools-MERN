3feccfbeb8de6194dee5efac40dffc95f5afde2d
HardikArora0843 <118887883+HardikArora0843@users.noreply.github.com>
2024-06-21 11:54:08 +0530

    Add files via upload

---
+++ WEEK 3/Lecture-23-CipherSchools/second-app/src/components/AddTask.js
@@ -0,0 +1,65 @@
+
+// ui form FROM SEMANTIC UI
+
+import { useState } from "react";
+
+const AddTask = () => {
+
+    {/****  WE HAVE CREATED A FUNCION BY WHICH IT WILL CONSOLE THE VALUE OF TEXT WE WILL WRITE IN INPUT BOX  ***/}
+
+    const [task, setTask] = useState({
+        title:"",
+        description: "",
+    });
+
+    let handleInputChange = (e) => {
+        // console.log(e.target.value);
+
+        setTask(
+            {
+                ...task,
+                [e.target.name]: e.target.value,
+            }
+        )
+    }
+
+    {/**** WE DONT WANT OUR FORM TO SUBMIT INFORMATION TO BROWSER THERFORE WE ARE DOING THIS TO PREVENT DEFAULT FEATURE */}
+
+    let onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(task);
+    }
+
+    return (
+    // <p>This is add task</p>
+
+    <>
+
+    <h3 className="ui heading center">Add New Task</h3>
+
+    <div className="ui form">
+
+        <form onSubmit={onFormSubmit}>
+            <div className="field">
+                <label>Title</label>
+                <input type="text" fdprocessedid="1qut7" placeholder="Task Title" name="title" onChange={handleInputChange}/>
+            </div>
+
+            <div className="field">
+                <label>Description</label>
+                <textarea rows="2" placeholder="Task Description" name="description" onChange={handleInputChange}></textarea>
+            </div>
+
+            <button type="submit" className="ui primary button" fdprocessedid="jhz0rs"> Submit </button>
+
+        </form>
+
+    </div>
+
+    </>
+
+    )
+}
+
+export default AddTask;
+
